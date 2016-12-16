7373e06522bd671ce78423a7fa98ae1393184c46
danielfc <daniel.felicissimo@gmail.com>
2016-12-16 17:26:05 -0200

    Add common contractions file

--- trivor-engine/src/main/java/com/kalnee/trivor/engine/TrivorEngineApplication.java
+++ trivor-engine/src/main/java/com/kalnee/trivor/engine/TrivorEngineApplication.java
@@ -1,6 +1,7 @@
 package com.kalnee.trivor.engine;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.CommandLineRunner;
@@ -21,18 +22,24 @@ public class TrivorEngineApplication {
   @Bean
   InitializingBean seedMongoDB(SubtitleRepository subtitleRepository) {
     return () -> {
-    	subtitleRepository.deleteAll();
+      subtitleRepository.deleteAll();
       subtitleRepository.insert(new SubtitleModel(100L, "Flash", 1, 10, 2013,
-          Arrays.asList("Hey, buddy!", "What's up?", "What are you doing over here?")));
+          Arrays.asList("That's nice.", "Don't move, please.", "Nobody's gonna see my feet.")));
       subtitleRepository.insert(new SubtitleModel(100L, "Flash", 1, 11, 2013,
-          Arrays.asList("Did I do something wrong?", "Come on!", "What?")));
-			subtitleRepository.insert(new SubtitleModel(1200L, "Dexter", 2, 4, 2011,
-				Arrays.asList("I want to kill again.", "Should I?", "Oh, yeah!")));
+          Arrays.asList("Why are you insisting on doing this?", "Come on!", "What?")));
+      subtitleRepository.insert(new SubtitleModel(1200L, "Dexter", 2, 4, 2011,
+          Arrays.asList("I want to kill again.", "Should I?", "Oh, yeah!")));
     };
   }
 
   @Bean
   CommandLineRunner runner(SubtitleRepository subtitleRepository) {
-    return (args) -> subtitleRepository.findByYear(2013).forEach(System.err::println);
+    return (args) -> {
+			System.err.println("Phrases that use contractions:");
+			subtitleRepository.findByYear(2013).stream()
+				.flatMap(s -> s.getPhrases().stream())
+				.filter(s -> s.contains("'"))
+				.forEach(System.err::println);
+		};
   }
 }
