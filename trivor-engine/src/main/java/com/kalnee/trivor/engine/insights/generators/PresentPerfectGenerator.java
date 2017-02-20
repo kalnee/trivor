053bba7dc6299b4fad53b5802a2d80764dab8d76
João Mariano <joaocar@gmail.com>
2017-02-20 09:22:35 -0300

    Fix Present Perfect tests

--- trivor-engine/src/main/java/com/kalnee/trivor/engine/insights/generators/PresentPerfectGenerator.java
+++ trivor-engine/src/main/java/com/kalnee/trivor/engine/insights/generators/PresentPerfectGenerator.java
@@ -1,19 +1,18 @@
 package com.kalnee.trivor.engine.insights.generators;
 
-import static com.kalnee.trivor.engine.utils.CollectionUtils.*;
-import static com.kalnee.trivor.engine.utils.TagsEnum.*;
-import static java.lang.String.format;
-import static java.util.stream.Collectors.toList;
+import com.kalnee.trivor.engine.models.Insight;
+import com.kalnee.trivor.engine.models.Sentence;
+import com.kalnee.trivor.engine.models.Subtitle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.kalnee.trivor.engine.models.Insight;
-import com.kalnee.trivor.engine.models.Sentence;
-import com.kalnee.trivor.engine.models.Subtitle;
+import static com.kalnee.trivor.engine.utils.CollectionUtils.*;
+import static com.kalnee.trivor.engine.utils.TagsEnum.*;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 
 public class PresentPerfectGenerator implements InsightGenerator<List<String>> {
 
@@ -26,6 +25,9 @@ public class PresentPerfectGenerator implements InsightGenerator<List<String>> {
 	private static final List<String> MUST_CONTAIN_3RD = Arrays.asList(VBZ.name(), VBN.name(), PRP.name());
 	private static final List<String> MUST_NOT_CONTAIN = Arrays.asList(
 		VBG.name(), VBD.name()
+	);
+	private static final List<String> MUST_NOT_CONTAIN_MODAL = Arrays.asList(
+            "will", "Will", "won't", "Won't", "'ll"
 	);
 
 	@Override
@@ -43,7 +45,8 @@ public class PresentPerfectGenerator implements InsightGenerator<List<String>> {
 			.stream()
 			.filter(s -> anyMatch(s.getSentence(), MUST_CONTAIN)
 				&& (allMatch(s.getSentenceTags(), MUST_CONTAIN_NON_3RD) || allMatch(s.getSentenceTags(), MUST_CONTAIN_3RD))
-				&& noneMatch(s.getSentenceTags(), MUST_NOT_CONTAIN))
+				&& noneMatch(s.getSentenceTags(), MUST_NOT_CONTAIN)
+                && anyMatch(s.getSentence(), MUST_NOT_CONTAIN_MODAL))
 			.map(Sentence::getSentence)
 			.collect(toList());
 
