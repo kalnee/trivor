78be5f1782fa6474160d3d68f2e8898faa06ffc4
danielfc <daniel.felicissimo@gmail.com>
2017-08-23 19:26:07 -0300

    Swagger Doc Tweaks
    - Upgrade swagger libs
    - Configure swagger generator
    - Set API context to /api/v1

--- trivor-insights/src/main/java/org/kalnee/trivor/insights/config/SwaggerConfig.java
+++ trivor-insights/src/main/java/org/kalnee/trivor/insights/config/SwaggerConfig.java
@@ -3,12 +3,15 @@ package org.kalnee.trivor.insights.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
 import static springfox.documentation.builders.RequestHandlerSelectors.any;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
@@ -17,24 +20,25 @@ import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 @Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class SwaggerConfig {
 
-  @Bean
-  public Docket api() {
-    return new Docket(SWAGGER_2)
-        .select()
-        .apis(any())
-        .paths(PathSelectors.any())
-        .build()
-        .apiInfo(apiInfo());
-  }
+    @Bean
+    public Docket api() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(any())
+                .paths(or(regex("/subtitles.*"), regex("/insights.*")))
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-  private ApiInfo apiInfo() {
-    return new ApiInfo("trivor-insights REST API",
-        "Documentation of the main endpoints of trivor-insights.",
-        "v1",
-        "Terms of service",
-        new Contact("Kalnee", "http://github.com/kalnee", "kalnee.trivor@gmail.com"),
-        "MIT",
-        "http://github.com/kalnee/trivor/LICENSE.md"
-    );
-  }
+    private ApiInfo apiInfo() {
+        return new ApiInfo("trivor-insights REST API",
+                "Documentation of the main endpoints of trivor-insights.",
+                "v1",
+                "Terms of service",
+                new Contact("Kalnee", "http://github.com/kalnee", "kalnee.trivor@gmail.com"),
+                "MIT",
+                "http://github.com/kalnee/trivor/LICENSE.md",
+                Collections.emptyList()
+        );
+    }
 }
